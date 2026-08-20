b183a56a9a9b339743d80a40841621ed469bb613
zzzhoo1 <gvfr2022@outlook.com>
2026-08-20 20:45:59 +0800

    feat(ui): add semantic light and dark theme tokens

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -2,6 +2,8 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
 import {i18n} from "./i18n";
+import "./styles/tokens.css";
+import "./styles/themes.css";
 import "./styles/main.css";
 
 createApp(App).use(router).use(i18n).mount("#app");
--- server-ui/src/main.ts
+++ server-ui/src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import {router} from './router'
 import {createPinia} from 'pinia'
 import {i18n} from './i18n'
+import './assets/styles/tokens.css'
+import './assets/styles/themes.css'
 import './assets/styles/main.css'
 
 createApp(App).use(createPinia()).use(router).use(i18n).mount('#app')
